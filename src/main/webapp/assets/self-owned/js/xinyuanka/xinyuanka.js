5a59842c6aa44dc9b2deffc8caee5c172af1486d
zoudan <zoudan@kuanguang.local>
2016-09-20 23:58:59 +0800

    xinyuanka_3

--- src/main/webapp/assets/self-owned/js/xinyuanka/xinyuanka.js
+++ src/main/webapp/assets/self-owned/js/xinyuanka/xinyuanka.js
@@ -21,24 +21,45 @@ function chooseType(type) {
     console.log("choose type in and type = " + type)
 
     xinyuanka_3.dream_type(type);
-    
-    $('#whole_page').css('background', 'url(assets/self-owned/img/xinyuanka/'+type+'.jpg)');
-    
-//    switch (type) {
-//        case "plane":
-//            $('#body').css('background', 'url(assets/self-owned/img/xinyuanka/plane.jpg)');
-//            break;
-//        case "car":
-//            $('#body').css('background', 'url(assets/self-owned/img/xinyuanka/car.jpg)');
-//            break;
-//        case "house":
-//            $('#body').css('background', 'url(assets/self-owned/img/xinyuanka/house.jpg)');
-//            break;
-//        case "travel":
-//            $('#body').css('background', 'url(assets/self-owned/img/xinyuanka/p.jpg)');
-//            break;
-//        case "career":
-//            break;
-//        default:
-//    }
+
+//    $('#whole_page').css('background', 'url(assets/self-owned/img/xinyuanka/'+type+'.jpg)');
+
+    $('.img_size').css('width', "40px");
+    $('.img_size').css('height', "40px");
+
+//    $('#car_image').css('width', "40px");
+//    $('#car_image').css('height', "40px");
+//
+//    $('#house_image').css('width', "40px");
+//    $('#house_image').css('height', "40px");
+//
+//    $('#travel_image').css('width', "40px");
+//    $('#travel_image').css('height', "40px");
+//
+//    $('#career_image').css('width', "40px");
+//    $('#career_image').css('height', "40px");
+
+    switch (type) {
+        case "plane":
+            $('#plane_image').css('width', "60px");
+            $('#plane_image').css('height', "60px");
+            break;
+        case "car":
+            $('#car_image').css('width', "60px");
+            $('#car_image').css('height', "60px");
+            break;
+        case "house":
+            $('#house_image').css('width', "60px");
+            $('#house_image').css('height', "60px");
+            break;
+        case "travel":
+            $('#travel_image').css('width', "60px");
+            $('#travel_image').css('height', "60px");
+            break;
+        case "career":
+            $('#career_image').css('width', "60px");
+            $('#career_image').css('height', "60px");
+            break;
+        default:
+    }
 }
